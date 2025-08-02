93b0decbf6d924fe39a3225d2bad6b3eacfc0363
RMPlaysMCYT <ronnel.mitra.77@gmail.com>
2025-08-02 13:20:08 +0800

    Updated additional files

---
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
@@ -0,0 +1,5 @@
+import {system, world} from "@minecraft/server";
+world.afterEvents.entitySpawn.subscribe(({entity}) => {
+    if(entity?.typeId === "aspire:ambulance"){entity.nameTag('Ambulance')}
+    else if(entity?.typeId === "aspire:apv"){entity.nameTag('AUV Car')}
+});
