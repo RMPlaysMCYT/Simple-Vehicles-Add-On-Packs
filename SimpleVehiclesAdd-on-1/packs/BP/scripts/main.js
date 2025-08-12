9e01356552f13fdd4c8aaa8cb53d88426c9e1960
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-08-12 12:48:23 +0800

    Added Scripts and shit

--- SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
@@ -1,5 +1,24 @@
-import {system, world} from "@minecraft/server";
-world.afterEvents.entitySpawn.subscribe(({entity}) => {
-    if(entity?.typeId === "aspire:ambulance"){entity.nameTag('Ambulance')}
-    else if(entity?.typeId === "aspire:apv"){entity.nameTag('AUV Car')}
+import { system, world } from "@minecraft/server";
+
+world.afterEvents.entitySpawn.subscribe(({ entity }) => {
+  if (entity?.typeId === "aspire:ambulance") {
+    entity.nameTag("Ambulance");
+  } else if (entity?.typeId === "aspire:apv") {
+    entity.nameTag("AUV Car");
+  }
 });
+
+function VehicleHudHider() {
+  world.getAllPlayers().forEach((player) => {
+    const ride = player.getComponent("minecraft:rideable")?.entityRidingOn;
+    const commando =
+      ride && ride.typeId.startsWith("aspire")
+        ? "hud @s hide horse_health"
+        : "hud @s reset horse_health";
+    player.runCommand(commando);
+  });
+}
+
+system.runInterval(() => {
+  VehicleHudHider();
+}, 1);
