da12c78d827018709d3d183693f02dd0994f2fd5
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-08-12 13:45:46 +0800

    Names has been fixed

--- SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
@@ -1,8 +1,8 @@
-import '_hud.js';
+import '_hud';
 
 import { system, world } from "@minecraft/server";
 
 world.afterEvents.entitySpawn.subscribe(({ entity }) => {
-  if (entity?.typeId === "aspire:ambulance") {entity.nameTag("Ambulance")}
-  else if (entity?.typeId === "aspire:apv") {entity.nameTag("AUV Car");}
+  if (entity?.typeId === "aspire:ambulance") {entity.nameTag = "Ambulance"}
+  else if (entity?.typeId === "aspire:apv") {entity.nameTag = "AUV Car"}
 });
