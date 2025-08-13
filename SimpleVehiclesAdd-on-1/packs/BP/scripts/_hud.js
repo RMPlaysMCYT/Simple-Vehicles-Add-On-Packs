41f5b69be10cea0ec8b72abb31932915c87851c2
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-08-13 21:57:23 +0800

    Moving several files to ensure compability especially in resources pack

--- SimpleVehiclesAdd-on-1/packs/BP/scripts/_hud.js
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/_hud.js
@@ -1,9 +1,10 @@
-import {world, system} from '@minecraft/server';
+import { world, system } from "@minecraft/server";
 
 function VehicleHudHider() {
   world.getAllPlayers().forEach((player) => {
     const rider = player.getComponent("minecraft:riding")?.entityRidingOn;
-    const commando = rider && rider.typeId.startsWith("aspire")
+    const commando =
+      rider && rider.typeId.startsWith("aspire")
         ? `hud @s hide horse_health`
         : `hud @s reset horse_health`;
     player.runCommand(commando);
--- SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/main.js
@@ -1,8 +1,11 @@
-import './_hud';
+import "./_hud";
 
 import { system, world } from "@minecraft/server";
 
 world.afterEvents.entitySpawn.subscribe(({ entity }) => {
-  if (entity?.typeId === "aspire:ambulance") {entity.nameTag = "Ambulance"}
-  else if (entity?.typeId === "aspire:apv") {entity.nameTag = "AUV Car"}
+  if (entity?.typeId === "aspire:ambulance") {
+    entity.nameTag = "Ambulance";
+  } else if (entity?.typeId === "aspire:apv") {
+    entity.nameTag = "AUV Car";
+  }
 });
