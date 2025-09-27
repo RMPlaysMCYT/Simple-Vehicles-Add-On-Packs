2e22d16afac8f7625513f31729dfb8e9472ba036
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-27 20:04:18 +0800

    another test

--- SimpleVehiclesAddOn-172Final/packs/BP/scripts/honkItem.js
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/honkItem.js
@@ -5,6 +5,6 @@ world.beforeEvents.itemUse.subscribe(({source, ItemStack})=>{
     const iTems = ItemStack;
     if (!iTems) return;
     if (iTems.typeId === "simple_vehicles:honk_item"){
-        source.playSound('vehicles.horn', 1, 1);
+        source.playSound("random.toast", 1, 1);
     }
 })
