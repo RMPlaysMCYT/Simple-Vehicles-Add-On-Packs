f7489ca58404d624ea7aea9ef75bf31288652f9d
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-27 21:47:58 +0800

    here I fixed it haha

--- SimpleVehiclesAddOn-172Final/packs/BP/scripts/setLoreInfo.js
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/setLoreInfo.js
@@ -1,15 +1,5 @@
 import { system, world } from "@minecraft/server";
-
-const CUSTOM_ITEMS = {
-  "simple_vehicles:honk_item": [
-    "This is the first line of the item's lore",
-    "This is the second line of the item's lore",
-    "You can only have 20 lines and 50 characters"
-  ],
-  "simple_vehicles:key": [
-    "This is an required item tho for every vehicle"
-  ]
-};
+import { CUSTOM_ITEMS } from "./utils/customItems";
 
 function updatePlayerInventoryLore() {
   for (const player of world.getPlayers()) {
---
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/utils/customItems.js
@@ -0,0 +1,10 @@
+export const CUSTOM_ITEMS = {
+  "simple_vehicles:honk_item": [
+    "This is the first line of the item's lore",
+    "This is the second line of the item's lore",
+    "You can only have 20 lines and 50 characters"
+  ],
+  "simple_vehicles:key": [
+    "This is an required item tho for every vehicle"
+  ]
+};
