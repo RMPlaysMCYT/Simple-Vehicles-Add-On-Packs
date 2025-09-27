9f2e9d6dbf0bc89b33668d7ef470ec428760670b
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-27 20:16:45 +0800

    another fix yet to be followed if there's freaking error occurs

--- SimpleVehiclesAddOn-172Final/packs/BP/scripts/honkItem.js
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/honkItem.js
@@ -1,10 +1,30 @@
 import {world, system, ItemComponentTypes, ItemStack } from '@minecraft/server';
 
+world.afterEvents.itemCompleteUse.subscribe(eventData => {
+    const { source, itemStack } = eventData;
+    const item = itemStack;
 
-world.afterEvents.itemCompleteUse.subscribe(({source, ItemStack})=>{
-    const iTems = ItemStack;
-    if (!iTems) return;
-    if (iTems.typeId === "simple_vehicles:honk_item"){
-        source.playSound("random.toast", 1, 1);
+    if (!item) return;
+
+    if (item.typeId === "simple_vehicles:honk_item") {
+        source.playSound("random.toast", { volume: 1, pitch: 1 });
     }
 });
+
+// import { system, world, ItemComponentTypes, ItemStack } from "@minecraft/server";
+
+// world.afterEvents.itemCompleteUse.subscribe(({source, itemStack }) => {
+  
+//   const item = itemStack;
+
+//   if (!item) return;
+  
+//   if (item.typeId === "beest:arowana") {
+//   source.addEffect('fire_resistance', 200, {amplifier:1});
+// source.addEffect('night_vision', 300, {amplifier:1});
+//   }
+  
+//   if (item.typeId === "beest:zebra_fish") {
+//   source.runCommand(`effect @s resistance 30 2`);
+//   }
+// });
