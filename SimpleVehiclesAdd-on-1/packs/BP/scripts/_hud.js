8c64bce41a3e78c4046c231505f8a7439c73b3fa
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-10-23 05:01:40 +0800

    fixing the _hud.js and add vehiclePrefixes but any add-ons can be compatible with

--- SimpleVehiclesAdd-on-1/packs/BP/scripts/_hud.js
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/_hud.js
@@ -1,10 +1,15 @@
 import { world, system } from "@minecraft/server";
 
+const vehiclePrefixes = [
+  "simple_vehicles",  
+  "aspire"           
+];
+
 function VehicleHudHider() {
   world.getAllPlayers().forEach((player) => {
     const rider = player.getComponent("minecraft:riding")?.entityRidingOn;
     const commando =
-      rider && rider.typeId.startsWith("aspire")
+      rider && vehiclePrefixes.some(prefix => rider.typeId.startsWith(prefix))
         ? `hud @s hide horse_health`
         : `hud @s reset horse_health`;
     player.runCommand(commando);
