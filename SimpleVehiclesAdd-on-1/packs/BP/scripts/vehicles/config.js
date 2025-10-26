28d47bd95ca66dd377a6113b8a163b76b1fc7b09
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-10-27 06:29:26 +0800

    Add files via upload, some files can't upload auto
    add files manually

---
+++ SimpleVehiclesAdd-on-1/packs/BP/scripts/vehicles/config.js
@@ -0,0 +1,34 @@
+export const Ambulance = {
+    typeId: "aspire:ambulance",
+    optiones: {
+        vehicleGroundable: {},
+        fuelSize: {
+            fuelMax: 4000,
+            fuelInitial: 4000,
+            burnRate: 1
+        }
+    },
+    dictionaryVehicleInfo: {
+        bio: "Ambulance",
+        speed: "Mid",
+        seatCapacity: 2
+    }
+}
+
+
+export const APV = {
+    typeId: "aspire:apv",
+    optiones: {
+        vehicleGroundable: {},
+        fuelSize: {
+            fuelMax: 4000,
+            fuelInitial: 4000,
+            burnRate: 1
+        }
+    },
+    dictionaryVehicleInfo: {
+        bio: "Ambulance",
+        speed: "Mid",
+        seatCapacity: 4
+    }
+}
