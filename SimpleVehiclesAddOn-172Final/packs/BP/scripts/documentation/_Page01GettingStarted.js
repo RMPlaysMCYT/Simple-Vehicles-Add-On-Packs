00ace63efd87e1156201ae3f838418a251299468
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-11-21 11:39:02 +0800

    few stuff was been added for now

--- SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_Page01GettingStarted.js
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_Page01GettingStarted.js
@@ -1,20 +1,26 @@
-import { ActionFormData } from "@minecraft/server-ui"
+import { ActionFormData } from "@minecraft/server-ui";
 
-import {showCustomForm} from "./MainMenu"
+import { showCustomForm } from "./MainMenu";
 
-export function _Page1GettingStarted(player){
-    const CreditsForm = new ActionFormData();
-    CreditsForm.title('Credits')
-    CreditsForm.body(
-        { "rawtext": [{ "text": "simveh172.text1" }, { "text": "\n" }, { "text": "simveh172.text2" }, { "text": "\n" }, { "text": "===============================" }, { "text": "\n" }] })
-    CreditsForm.button("Go Back");
-    CreditsForm.show(player).then(
-        (response) => {
-            if (response.canceled) {
-                showCustomForm(player,);
-            } else if (response.selection === 0) {
-                showCustomForm(player); 
-            }
-        }
-    );
+export function _Page1GettingStarted(player) {
+  const CreditsForm = new ActionFormData();
+  CreditsForm.title("Credits");
+  CreditsForm.body({
+    rawtext: [
+      { text: "simveh172.text1" },
+      { text: "\n" },
+      { text: "simveh172.text2" },
+      { text: "\n" },
+      { text: "===============================" },
+      { text: "\n" },
+    ],
+  });
+  CreditsForm.button("Go Back");
+  CreditsForm.show(player).then((response) => {
+    if (response.canceled) {
+      showCustomForm(player);
+    } else if (response.selection === 0) {
+      showCustomForm(player);
+    }
+  });
 }
