9fc41ffff98ed6410c9501442d4b192cedd77770
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-12-25 00:18:54 +0800

    add new changes and still find the way to fix it

--- SimpleVehiclesAddOn-172Final/packs/BP/scripts/main.js
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/main.js
@@ -2,17 +2,37 @@ const Version = "172";
 console.log("Addon Loaded")
 console.log(`Simple Vehicles Addon ${Version} Loaded`)
 
+import * as btch from '@minecraft/server';
 import { system, world } from "@minecraft/server";
-import "./booksGuide";
 import "./documentation/MainMenu";
-import "./honkItem";
-import "./setLoreInfo";
-import "./_hud";
+import "honkitem";
+import "setLoreInfo";
+import "_hud";
 
 import * as fck from "./itemInteraction/playerOnEnter"
 
 let onWorldStartedSetup = false;
 let onWorldFinishedSetup = false;
+
+
+const DEBUGASSHOLE = false;
+
+var NameSpaces = "SimpleVehiclesAddOn172";
+var NamingBtch = "SimpleVehiclesAddOn/172";
+
+function givePlayerBook(player){
+    player.runCommand(`execute as @s[tag=!${NameSpaces.substring(0,NamingBtch.length -1)}] at @s run give @s simple_vehicles:book_documents`);
+    player.runCommand(`tag @s[tag=!${NameSpaces.substring(0,NamingBtch.length -1)}] add ${NameSpaces.substring(0,NamingBtch.length -1)}`);
+    if (DEBUGASSHOLE){
+        btch.world.sendMessage(`Given Player Book`);
+    }
+}
+
+btch.world.afterEvents.playerSpawn.subscribe((event)=>{
+    const shit = event;
+    givePlayerBook(shit.player);
+})
+
 
 async function Ticks() {
   await fck.onWorldTicks();
