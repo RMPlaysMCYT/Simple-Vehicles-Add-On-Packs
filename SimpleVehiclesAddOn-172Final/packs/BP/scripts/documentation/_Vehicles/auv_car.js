21bad99660ec4f400e36891ad3c57e0f19316160
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-01-04 16:24:30 +0800

    add some documentations to these specific vehicles

---
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_Vehicles/auv_car.js
@@ -0,0 +1,30 @@
+import { ActionFormData } from "@minecraft/server-ui";
+import { showCustomForm } from "../mainmenu";
+import { HealthTypes, VehicleSizes } from "../../utils/customvehicleitems";
+import { _Page2Vehicles } from "../_page2";
+
+export function AUVCarInformation(player) {
+  const CreditsForm = new ActionFormData();
+  CreditsForm.title("AUV Car");
+  CreditsForm.body({
+    rawtext: [
+      {
+        text: "The AE86 series of the Toyota Corolla Levin/Sprinter Trueno are small, front-engine/rear-wheel-drive compact cars within the mostly front-engine/front-wheel-drive fifth generation Corolla (E80) range—marketed and manufactured by Toyota from 1983 to 1987 in coupé and liftback configurations.",
+      },
+      { text: "\n" },
+      { text: "===============================" },
+      { text: "Specifictaions \n" },
+      { text: `\xA7lSpeed: \xA7r120 km/h \n` },
+      { text: `\xA7lSize: \xA7r${VehicleSizes[0]} \n` },
+      { text: `\xA7lHealth: \xA7r${HealthTypes[2]}` },
+    ],
+  });
+  CreditsForm.button("Go Back");
+  CreditsForm.show(player).then((response) => {
+    if (response.canceled) {
+      showCustomForm(player);
+    } else if (response.selection === 0) {
+      _Page2Vehicles(player);
+    }
+  });
+}
