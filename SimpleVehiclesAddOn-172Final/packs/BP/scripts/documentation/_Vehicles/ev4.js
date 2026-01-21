1ee0ef915dc15b3d8418d9270e295da1445b89eb
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-01-21 14:25:09 +0800

    add ve4 and fire truck

---
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_Vehicles/ev4.js
@@ -0,0 +1,30 @@
+import { ActionFormData } from "@minecraft/server-ui";
+import { showCustomForm } from "../mainmenu";
+import { HealthTypes, VehicleSizes } from "../../utils/customvehicleitems";
+import { _Page2Vehicles } from "../_page2";
+
+export function CorllaInformation(player) {
+  const CreditsForm = new ActionFormData();
+  CreditsForm.title("EV4");
+  CreditsForm.body({
+    rawtext: [
+      {
+        text: "The EV4 Is a fictional Concept From the Simple Vehicles Add-on, it is an electric vehicle with decent speed and acceleration, making it a fun choice for players who enjoy zipping around in a futuristic ride.",
+      },
+      { text: "\n" },
+      { text: "===============================" },
+      { text: "Specifictaions \n" },
+      { text: `\xA7lSpeed: \xA7r120 km/h \n` },
+      { text: `\xA7lSize: \xA7r${VehicleSizes[0]} \n` },
+      { text: `\xA7lHealth: \xA7r${HealthTypes[2]}` },
+    ],
+  });
+  CreditsForm.button("Go Back");
+  CreditsForm.show(player).then((response) => {
+    if (response.canceled) {
+      showCustomForm(player);
+    } else if (response.selection === 0) {
+      _Page2Vehicles(player);
+    }
+  });
+}
