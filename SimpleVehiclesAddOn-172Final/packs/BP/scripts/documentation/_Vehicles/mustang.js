f56751545a5b78dd6803bc20563f8ea0b4f790b4
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-02-02 13:25:45 +0800

    Add New Vehicle Information

---
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_Vehicles/mustang.js
@@ -0,0 +1,30 @@
+import { ActionFormData } from "@minecraft/server-ui";
+import { showCustomForm } from "../mainmenu";
+import { HealthTypes, VehicleSizes } from "../../utils/customvehicleitems";
+import { _Page2Vehicles } from "../_page2";
+
+export function Mustang_Information(player) {
+  const CreditsForm = new ActionFormData();
+  CreditsForm.title("For Mustang");
+  CreditsForm.body({
+    rawtext: [
+      {
+        text: "A limousine (/ˈlɪməziːn/ or /lɪməˈziːn/), or limo (/ˈlɪmoʊ/) for short,[1] is a large, chauffeur-driven luxury vehicle with a partition between the driver compartment and the passenger compartment which can be operated mechanically by hand or by a button electronically.[2] A luxury sedan with a very long wheelbase and driven by a professional driver is called a stretch limousine.[3]",
+      },
+      { text: "\n" },
+      { text: "===============================" },
+      { text: "Specifictaions \n" },
+      { text: `\xA7lSpeed: \xA7r120 km/h \n` },
+      { text: `\xA7lSize: \xA7r${VehicleSizes[0]} \n` },
+      { text: `\xA7lHealth: \xA7r${HealthTypes[2]}` },
+    ],
+  });
+  CreditsForm.button("Go Back");
+  CreditsForm.show(player).then((response) => {
+    if (response.canceled) {
+      showCustomForm(player);
+    } else if (response.selection === 0) {
+      _Page2Vehicles(player);
+    }
+  });
+}
--- SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_page2.js
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/documentation/_page2.js
@@ -15,6 +15,8 @@ import { Helicopter_Information } from "./_vehicles/helicopter";
 import { Hoverboard_Information } from "./_vehicles/hoverboarrd";
 import { Jeep_Information } from "./_vehicles/jeep";
 import { Limousine_Information } from "./_vehicles/limousine";
+import { Lowrider_Information } from "./_vehicles/lowrider";
+import { Mustang_Information } from "./_vehicles/mustang";
 
 export function _Page2Vehicles(player) {
   const Page2Vehicles = new ActionFormData();
@@ -23,7 +25,9 @@ export function _Page2Vehicles(player) {
     rawtext: [
       { text: "These are the Vehicles Which were been added to the add-on" },
       { text: "\n" },
-      { text: "It has 70 vehicles overall and it's been like quantity over quality for some, however it's been better than expected unlike other add-ons where it has 30+ or what" },
+      {
+        text: "It has 70 vehicles overall and it's been like quantity over quality for some, however it's been better than expected unlike other add-ons where it has 30+ or what",
+      },
       { text: "\n" },
       { text: "===============================" },
       { text: "\n" },
@@ -35,7 +39,7 @@ export function _Page2Vehicles(player) {
   Page2Vehicles.button("Bike", "textures/items/bike"); //3
   Page2Vehicles.button("Bus", "textures/items/bus"); //4
   Page2Vehicles.button("Sled/Sleigh", "textures/items/sled"); //5
-  Page2Vehicles.button("Car", "textures/items/car"); //6 
+  Page2Vehicles.button("Car", "textures/items/car"); //6
   Page2Vehicles.button("Car 4X4", "textures/items/car4x4"); //7
   Page2Vehicles.button("Toyota Corolla", "textures/items/corolla"); //8
   Page2Vehicles.button("EV4", "textures/items/ev4"); //9
@@ -44,7 +48,9 @@ export function _Page2Vehicles(player) {
   Page2Vehicles.button("Hoverboard", "textures/items/hoverboard"); //12
   Page2Vehicles.button("Jeep or Jeepney", "textures/items/jeep"); //13
   Page2Vehicles.button("Limousine", "textures/items/limousine"); //14
-  Page2Vehicles.button("Go Back"); //15
+  Page2Vehicles.button("Lowrider", "textures/items/lowrider"); //15
+  Page2Vehicles.button("Mustang", "textures/items/mustang"); //16
+  Page2Vehicles.button("Go Back"); //17
   Page2Vehicles.show(player).then((response) => {
     if (response.canceled) {
       showCustomForm(player);
@@ -74,13 +80,15 @@ export function _Page2Vehicles(player) {
       Helicopter_Information(player);
     } else if (response.selection === 12) {
       Hoverboard_Information(player);
-    }
-    else if (response.selection === 13) {
+    } else if (response.selection === 13) {
       Jeep_Information(player);
-    }
-    else if (response.selection === 14) {
+    } else if (response.selection === 14) {
       Limousine_Information(player);
     } else if (response.selection === 15) {
+      Lowrider_Information(player);
+    } else if (response.selection === 16) {
+      Mustang_Information(player);
+    } else if (response.selection === 17) {
       showCustomForm(player);
     }
   });
