71fd8a5c895c7f440cf14a94ee58f47e7da4ccb1
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-05-27 16:06:38 +0800

    add vehicleControl module

---
+++ SimpleVehiclesAddOn-172Final/packs/BP/scripts/utils/vehicleControls.js
@@ -0,0 +1,46 @@
+const VehicleControlItemSetPacks = {
+  "simple_vehicles:ae86": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+    "simple_vehicles:tool_window_open",
+  ],
+  "simple_vehicles:ambulance": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:apv": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:bus_2": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:bus": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:apv": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:car": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:corolla": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+  "simple_vehicles:ev_4": [
+    "simple_vehicles:honk_item",
+    "simple_vehicles:key",
+  ],
+};
+
+const SimVehAllControls = new Set([
+  "simple_vehicles:honk_item",
+  "simple_vehicles:key",
+  "simple_vehicles:tool_window_open",
+  "simple_vehicles:tool_windshield",
+]);
